c90d6edde1423dec5479a8aeb96478b5b23d71bc
mehmettbaki <mehmetbaki123@gmail.com>
2024-01-16 15:21:04 +0300

    create session

--- app.py
+++ app.py
@@ -18,34 +18,32 @@ engine = AIOEngine()
 
 @app.post("/trees/", response_model=Tree)
 async def postTree(tree: Tree):
-    await engine.save(tree)
-    return tree
-
-
-@app.put("/trees/", response_model=Tree)
-async def create_tree(tree: Tree):
-    await engine.save(tree)
-    return tree
+    async with engine.session() as session:
+        await session.save(tree)
+        return tree
 
 
 @app.get("/trees/", response_model=List[Tree])
 async def get_trees():
-    trees = await engine.find(Tree)
-    return trees
+    async with engine.session() as session:
+        trees = await session.find(Tree)
+        return trees
 
 
 @app.get("/trees/count", response_model=int)
 async def count_trees():
-    count = await engine.count(Tree)
-    return count
+    async with engine.session() as session:
+        count = await session.count(Tree)
+        return count
 
 
 @app.get("/trees/{id}", response_model=Tree)
 async def get_tree_by_id(id: ObjectId):
-    tree = await engine.find_one(Tree, Tree.id == id)
-    if tree is None:
-        raise HTTPException(404)
-    return tree
+    async with engine.session() as session:
+        tree = await session.find_one(Tree, Tree.id == id)
+        if tree is None:
+            raise HTTPException(404)
+        return tree
 
 
 if __name__ == "__main__":
